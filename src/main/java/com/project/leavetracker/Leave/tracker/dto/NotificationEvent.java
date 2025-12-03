e49ff7b89f535a23478e42664a84135afe9ac734
sachinpalcs <sachin004pal@gmail.com>
2025-12-03 23:44:12 +0530

    Notification Event add

---
+++ src/main/java/com/project/leavetracker/Leave/tracker/dto/NotificationEvent.java
@@ -0,0 +1,15 @@
+package com.project.leavetracker.Leave.tracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationEvent {
+    private String recipientEmail;
+    private String subject;
+    private String message;
+    private Long leaveRequestId;
+}
