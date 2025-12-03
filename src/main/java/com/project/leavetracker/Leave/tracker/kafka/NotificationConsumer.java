89ee142fcd81a1a5e42e8a9cdf1a380d341bdd0a
sachinpalcs <sachin004pal@gmail.com>
2025-12-03 23:49:56 +0530

    Notification Producer and Consumer added

--- src/main/java/com/project/leavetracker/Leave/tracker/kafka/NotificationConsumer.java
+++ src/main/java/com/project/leavetracker/Leave/tracker/kafka/NotificationConsumer.java
@@ -1,4 +1,22 @@
 package com.project.leavetracker.Leave.tracker.kafka;
 
+import com.project.leavetracker.Leave.tracker.dto.NotificationEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class NotificationConsumer {
+    @KafkaListener(topics = "leave-notifications", groupId = "leave-tracker-group")
+    public void handleNotification(NotificationEvent event) {
+        log.info("CONSUMED EVENT: Sending email to {}", event.getRecipientEmail());
+
+        try {
+            Thread.sleep(2000); // Sleep for 2 seconds to prove async works
+            log.info("Email sent successfully: {}", event.getSubject());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
--- src/main/java/com/project/leavetracker/Leave/tracker/kafka/NotificationProducer.java
+++ src/main/java/com/project/leavetracker/Leave/tracker/kafka/NotificationProducer.java
@@ -1,4 +1,20 @@
 package com.project.leavetracker.Leave.tracker.kafka;
 
+import com.project.leavetracker.Leave.tracker.dto.NotificationEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class NotificationProducer {
+    private final KafkaTemplate<String, NotificationEvent> kafkaTemplate;
+    private static final String TOPIC = "leave-notifications";
+
+    public void sendNotification(NotificationEvent event) {
+        log.info("Sending notification event to Kafka for request ID: {}", event.getLeaveRequestId());
+        kafkaTemplate.send(TOPIC, event);
+    }
 }
